a4cd6d1d7f70c3208aff24643589f1e9feafec1e
arungit2020 <aruprajapati9825@gmail.com>
2023-06-12 07:57:42 +0530

    intial  commt

---
+++ Basics/Intro.py
@@ -0,0 +1,18 @@
+print("Hello World")
+
+for i in range(10):
+    print("Hello World")
+
+i = 12
+my_sum = 9
+
+
+def summation(x):
+    return x * 10
+
+
+for i in range(100):
+    my_sum += i
+    i = i / summation(my_sum)
+
+print(my_sum)
